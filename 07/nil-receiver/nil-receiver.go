8fe74374276da7ed6da5cdcce4fc7093561c2025
jidicula <johanan@forcepush.tech>
2021-04-15 20:59:08 -0400

    feat(07): Example of a method handling a nil receiver

---
+++ 07/nil-receiver/nil-receiver.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+// Insert adds a value to a binary tree.
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
+	}
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+// Contains checks if val is in IntTree. Note that this method doesn't modify
+// the receiver, but handles a nil receiver.
+func (it *IntTree) Contains(val int) bool {
+	switch {
+	case it == nil:
+		return false
+	case val < it.val:
+		return it.left.Contains(val)
+	case val > it.val:
+		return it.right.Contains(val)
+	default:
+		return true
+	}
+}
+
+func main() {
+	var it *IntTree
+	it = it.Insert(5)
+	it = it.Insert(3)
+	it = it.Insert(10)
+	it = it.Insert(2)
+	fmt.Println(it.Contains(2))
+	fmt.Println(it.Contains(12))
+}
