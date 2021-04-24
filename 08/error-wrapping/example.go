6f2556b190fc22d7dc66d6c9e5dd87b9f0bb8a51
jidicula <johanan@forcepush.tech>
2021-04-24 15:17:42 -0400

    feat(08): Error wrapping example

---
+++ 08/error-wrapping/example.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// fileChecker checks if file exists.
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in fileChecker: %w", err)
+	}
+	f.Close()
+	return nil
+}
+
+func main() {
+	err := fileChecker("not_here.txt")
+	if err != nil {
+		fmt.Println(err)
+		if wrappedErr := errors.Unwrap(err); wrappedErr != nil {
+			fmt.Println(wrappedErr)
+		}
+	}
+}
