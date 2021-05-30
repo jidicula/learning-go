bdbe8836abe2d0ab770f6b660948a1099330c60d
jidicula <johanan@forcepush.tech>
2021-05-30 12:09:01 -0400

    feat(11): HTTP request multiplexing
    See https://www.alexedwards.net/blog/a-recap-of-request-handling

--- 11/http-server/example.go
+++ 11/http-server/example.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"time"
+	"os"
 )
 
 type HelloHandler struct {
@@ -14,17 +15,47 @@ func (hh HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	s := http.Server{
-		Handler:      HelloHandler{},
-		Addr:         "0.0.0.0/:8080",
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 90 * time.Second,
-		IdleTimeout:  120 * time.Second,
-	}
-	err := s.ListenAndServe()
-	if err != nil {
-		if err != http.ErrServerClosed {
-			panic(err)
+	// s := http.Server{
+	// 	Handler:      HelloHandler{},
+	// 	Addr:         ":8080",
+	// 	ReadTimeout:  30 * time.Second,
+	// 	WriteTimeout: 90 * time.Second,
+	// 	IdleTimeout:  120 * time.Second,
+	// }
+	// err := s.ListenAndServe()
+	// if err != nil {
+	// 	if err != http.ErrServerClosed {
+	// 		panic(err)
+	// 	}
+	// }
+
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello!\n"))
+	// })
+
+	person := http.NewServeMux()
+	person.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		nBytesWritten, err := w.Write([]byte("greetings!\n"))
+		if err != nil || nBytesWritten == 0 {
+			fmt.Printf("Error!\n")
+			os.Exit(1)
 		}
+	})
+	dog := http.NewServeMux()
+	dog.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		nBytesWritten, err := w.Write([]byte("good puppy!\n"))
+		if err != nil || nBytesWritten == 0 {
+			fmt.Printf("Error!\n")
+			os.Exit(1)
+		}
+	})
+
+	mux := http.NewServeMux()
+	mux.Handle("/person/", http.StripPrefix("/person", person))
+	mux.Handle("/dog/", http.StripPrefix("/dog", dog))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
 	}
 }
